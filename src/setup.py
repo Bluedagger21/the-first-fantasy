d0c23c5fb3269de4ffdbe496166c10f762cbbebd
bluedagger21 <shobiubikadobi@gmail.com>
2023-03-02 00:29:05 -0800

    updated py2exe setup, created distributable

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
 setup(console=[{"script":"main.py",
-          "dest_base": "TheFirstFantasy"}],
+                "dest_base": "TheFirstFantasy"}],
+      zipfile=None,
       options={"py2exe": {
-          "dist_dir": "..\dist"}
+          "dist_dir": "..\dist",
+          "bundle_files": 1,
+          "compressed": True}
                }
       )
